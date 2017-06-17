b58ff098ae6d52d34520ae88671a3182ecfe7196
nikitaignatiev@gmail.com <nikitaignatiev@gmail.com>
2017-06-17 21:02:08 +0300

    Первоначальная настройка

--- Ajaro/settings.py
+++ Ajaro/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django_comments',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Ajaro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+SITE_ID = 1
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
--- Ajaro/urls.py
+++ Ajaro/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^djadmin/', admin.site.urls),
+    url(r'^login/', views.login, name='login'),
+    url(r'^logout/', views.logout, name='logout'),
 ]
