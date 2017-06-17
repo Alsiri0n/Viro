e64016199db519ab4f75deaabc49dfef1bf850a0
nikitaignatiev@gmail.com <nikitaignatiev@gmail.com>
2017-06-18 01:20:11 +0300

    Добавление базовой функциональности
    Добавлены шаблоны пиганиции и сообщений.

--- generic/mixins.py
+++ generic/mixins.py
@@ -6,3 +6,13 @@ class CategoryListMixin(ContextMixin):
         context = super(CategoryListMixin, self).get_context_data(**kwargs)
         context["current_url"] = self.request.path
         return context
+
+
+class PagNumberMixin(CategoryListMixin):
+    def get_context_data(self, **kwargs):
+        context = super(PagNumberMixin, self).get_context_data(**kwargs)
+        try:
+            context["pn"] = self.request.GET["page"]
+        except:
+            context["pn"] = "1"
+        return context
